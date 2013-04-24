f8f17a0212c630df0582cea918cbc3286cb5ef1d
Gregory Crosswhite <gcrosswhite@gmail.com>
2013-04-24 15:49:27 +1000

    Added an (optional) startup script.

---
+++ startup.js
@@ -0,0 +1,25 @@
+// Startup {{{
+window.addEventListener("load",function() {
+    (function() {
+        var resources = document.getElementById("resources")
+        var heading_template = document.getElementById("heading_template")
+        for(var i = 0; i < headings.length; ++i) {
+            var heading = headings[i]
+            var node = heading_template.cloneNode(false)
+            node.setAttribute("id",heading)
+            node.appendChild(document.createTextNode(heading))
+            resources.appendChild(node)
+        }
+    })()
+
+    initializeSlick([
+        hire("title_slide"),
+        "",
+        fadeOutAndFire(1,"title_slide"),
+        hireAndFadeInUseActors(0.5,
+            "standard_backdrop",
+            headings[nextHeadingIndex()]
+        ),
+    ].concat(script()))
+},false)
+// }}}
