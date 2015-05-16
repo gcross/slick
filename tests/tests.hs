b0933067c93f46c4d8491ae6ccc7823fbe6b7054
Gregory Crosswhite <gcroswhite@gmail.com>
2015-05-16 10:19:14 -0700

    Beefed up the singleton tests.

--- tests/tests.hs
+++ tests/tests.hs
@@ -19,43 +19,34 @@ testForNullAnimationWithCombiner combiner =
             testProperty "length 0" $ \(t::Int) (x::Int) →
                 fst (animationFunction t x animationCache) == x
 
-testMiddleOfSingleAnimationWithCombiner combiner =
-    case combiner [test_animation] of
-        Animation{..} →
-            testProperty "middle test" $ \(x::Int) → do
-                t ← choose (0,1)
-                return $ fst (animationFunction t x animationCache) == t+x
-
-testLeftClampingOfSingleAnimationWithCombiner combiner =
-    case combiner [test_animation] of
-        Animation{..} →
-            testProperty "middle test" $ \(x::Int) → do
-                t ← choose (-10,-1)
-                return $ fst (animationFunction t x animationCache) == x
-
-testRightClampingOfSingleAnimationWithCombiner combiner =
-    case combiner [test_animation] of
-        Animation{..} →
-            testProperty "middle test" $ \(x::Int) → do
-                t ← choose (2,10)
-                return $ fst (animationFunction t x animationCache) == 1+x
+testSingletonGroup combiner = testGroup "length 1" $
+    [testCase "correct duration" $
+        durationOf (combiner [test_animation]) @=? 1
+    ,testProperty "correct interior" $ \(x::Int) → do
+        t ← choose (0,1)
+        return $
+            case combiner [test_animation] of
+                Animation{..} → fst (animationFunction t x animationCache) == t+x
+    ,testProperty "correctly clamps the left" $ \(x::Int) → do
+        t ← choose (-10,-1)
+        return $
+            case combiner [test_animation] of
+                Animation{..} → fst (animationFunction t x animationCache) == x
+    ,testProperty "correctly clamps the right" $ \(x::Int) → do
+        t ← choose (2,10)
+        return $
+            case combiner [test_animation] of
+                Animation{..} → fst (animationFunction t x animationCache) == 1+x
+    ]
 
 tests =
     [testGroup "serial"
         [testForNullAnimationWithCombiner serial
-        ,testGroup "length 1"
-            [testMiddleOfSingleAnimationWithCombiner serial
-            ,testLeftClampingOfSingleAnimationWithCombiner serial
-            ,testRightClampingOfSingleAnimationWithCombiner serial
-            ]
+        ,testSingletonGroup serial
         ]
     ,testGroup "parallel"
         [testForNullAnimationWithCombiner parallel
-        ,testGroup "length 1"
-            [testMiddleOfSingleAnimationWithCombiner parallel
-            ,testLeftClampingOfSingleAnimationWithCombiner parallel
-            ,testRightClampingOfSingleAnimationWithCombiner parallel
-            ]
+        ,testSingletonGroup parallel
         ]
     ]
 
