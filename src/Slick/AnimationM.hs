e1fe1ab550a74f74761bbd775a44f9561f577c67
Gregory Crosswhite <gcrosswhite@gmail.com>
2015-10-10 14:30:23 -0700

    Now the animations are stored in a DList.

--- src/Slick/AnimationM.hs
+++ src/Slick/AnimationM.hs
@@ -9,6 +9,9 @@ module Slick.AnimationM where
 import Control.Lens (Lens',(^.),set)
 import Control.Monad.Trans.State (State,get,put,runState)
 
+import Data.DList (DList)
+import qualified Data.DList as DList
+import Data.Functor (fmap)
 import Data.Time.Clock (DiffTime)
 
 import Slick.Animation
@@ -16,7 +19,7 @@ import Slick.Animation
 data AnimationMState t s = AnimationMState
     { animationMTime :: t
     , animationMState :: s
-    , animationMAnimations :: [Animation t s]
+    , animationMAnimations :: DList (Animation t s)
     }
 
 type AnimationM t s = State (AnimationMState t s)
@@ -25,10 +28,10 @@ within :: Lens' s s' → AnimationM t s' α → AnimationM t s α
 within alens action = do
     old_state ← get
     let old_animation_state = animationMState old_state
-        (result,new_state) = runState action (old_state{animationMState = old_animation_state ^. alens, animationMAnimations=[]})
+        (result,new_state) = runState action (old_state{animationMState = old_animation_state ^. alens, animationMAnimations=DList.empty})
         new_animation_state = animationMState new_state
     put $ AnimationMState
         (animationMTime new_state)
         (set alens new_animation_state old_animation_state)
-        (animationMAnimations old_state ++ map (promoteAnimation alens) (animationMAnimations new_state))
+        (animationMAnimations old_state `DList.append` fmap (promoteAnimation alens) (animationMAnimations new_state))
     return result
